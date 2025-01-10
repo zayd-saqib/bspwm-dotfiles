c90ef5bcc7ba28d5617369d15589742821f13567
that guy <zaydsaqib20@gmail.com>
2025-01-10 16:43:02 +0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -32,29 +32,11 @@ set -l foreground c0caf5
     set -g fish_pager_color_completion $foreground
     set -g fish_pager_color_description $comment
     set -g fish_pager_color_selected_background --background=$selection
-#fm6000 --file ~/logo.txt --color red
-#alias fm6000="fm6000 --file ~/logo.txt --color red"
-# alias neofetch="neofetch --source ~/.config/neofetch/logo"
-#neofetch
-echo " "
-alias fetch="pokeget scizor --hide-name | fastfetch --file-raw -"
-alias mgba="mgba.sh"
-pokeget random --hide-name
-echo " "
+
 alias ls="exa --icons --color=always --group-directories-first"
-alias swaylock="swaylock --indicator-idle-visible -F -e --indicator-thickness 15 --inside-color 423732 --inside-wrong-color 533434 --layout-border-color 211915 --layout-bg-color 211915 --line-color 211915 --ring-color 211915 --key-hl-color 737942 --text-color d4be98 --text-wrong-color d4be98"
-alias pipes="pipes.sh"
-alias thsc="launch-thsc.sh"
-alias powermenu="rofi -show power-menu -modi power-menu:rofi-power-menu"
-alias unimatrix="unimatrix -s 96 -c magenta"
-alias legacy-launcher="legacy-launcher.sh"
-export XCURSOR_THEME="Bibata-Modern-Classic"
-export TERM=xterm-256color
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
 #export XDG_SESSION_TYPE=wayland
-export QT_QPA_PLATFORMTHEME=qt6ct
 starship init fish | source
-zoxide init fish | source
