9bf8e318fb5bf319aec6ef22577dbc26d194e54b
that guy <zaydsaqib20@gmail.com>
2025-01-14 22:25:04 +0400

    slight color and UI redesign

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,6 +33,7 @@ packer.startup(function(use)
 	use 'rcarriga/nvim-notify' -- dependency for noice.nvim
 	use 'folke/noice.nvim' -- noice.nvim
 	use 'folke/tokyonight.nvim' -- colorscheme
+	use 'rose-pine/neovim' -- colorscheme rose pine
 	use {
 		'nvim-lualine/lualine.nvim',
 	}
@@ -96,19 +97,19 @@ require('lualine').setup {
     icons_enabled = false,
     theme = {
 		normal = {
-			a = { fg = '#15161e', bg = "#7aa2f7" },
-			b = { fg = '#c0caf5', bg = '#0d0d18' },
-			c = { fg = '#c0caf5', bg = '#0d0d18' },
-			x = { fg = '#c0caf5', bg = '#0d0d18' },
-			y = { fg = '#20212d', bg = '#5a77b4' },
+			a = { fg = '#15161e', bg = "#7ca2f7" },
+			b = { fg = '#c2caf5', bg = '#100d18' },
+			c = { fg = '#c2caf5', bg = '#100d18' },
+			x = { fg = '#c2caf5', bg = '#100d18' },
+			y = { fg = '#20212d', bg = '#5c77b4' },
 		},
 		insert = {
-			a = { fg = '#15161e', bg = '#9ece6a' },
-			y = { fg = '#20212d', bg = '#6d8f48' }
+			a = { fg = '#15161e', bg = '#9fce6a' },
+			y = { fg = '#20212d', bg = '#6e8f48' }
 		},
 		visual = {
-			a = { fg = '#15161e', bg = '#a386d7' },
-			y = { fg = '#2a2137', bg = '#77639d' }
+			a = { fg = '#15161e', bg = '#a486d7' },
+			y = { fg = '#2a2137', bg = '#78639d' }
 		},
 	},
     component_separators = { left = '', right = ''},
@@ -149,6 +150,82 @@ require('lualine').setup {
   extensions = {}
 }
 -- set the color scheme and its options
+require("rose-pine").setup({
+    variant = "moon", -- auto, main, moon, or dawn
+    dark_variant = "moon", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    extend_background_behind_borders = true,
+
+    enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true, -- Handle deprecated options automatically
+    },
+
+    styles = {
+        bold = false,
+        italic = false,
+        transparency = true,
+    },
+
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
+
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+    },
+
+    palette = {
+        -- Override the builtin palette per variant
+        -- moon = {
+        --     base = '#18191a',
+        --     overlay = '#363738',
+        -- },
+    },
+
+    highlight_groups = {
+        -- Comment = { fg = "foam" },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+    },
+
+    before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end,
+})
+vim.cmd("colorscheme rose-pine")
+--[[
 require("tokyonight").setup({
   style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
@@ -196,19 +273,24 @@ require("tokyonight").setup({
     },
 })
 vim.cmd[[colorscheme tokyonight]]
+
 -- Set custom highlight for variables and other syntax elements
-vim.api.nvim_set_hl(0, '@variable', { fg = '#bb9af7' })
-vim.api.nvim_set_hl(0, '@string', { fg = '#9ece6a' })
+vim.api.nvim_set_hl(0, '@variable', { fg = '#c4a7e7' })
+vim.api.nvim_set_hl(0, '@string', { fg = '#9fce6a' })
 vim.api.nvim_set_hl(0, '@keyword.modifier', { fg = '#fab387' })
 vim.api.nvim_set_hl(0, '@function.macro', { fg = '#7aa2f7' })
-vim.api.nvim_set_hl(0, '@property', { fg = '#8f9dda' })
+vim.api.nvim_set_hl(0, '@property', { fg = '#a1a7d1' })
 vim.api.nvim_set_hl(0, '@variable.member', { fg = '#ff9e64' })
+vim.api.nvim_set_hl(0, "Title", { bold = false, fg = '#7aa2f7'}) -- for toml titles, etc
 --vim.api.nvim_set_hl(0, '@variable.member', { fg = '#74c7ec' })
 --vim.api.nvim_set_hl(0, '@punctuation.bracket', { fg = '#f9e2af' })
 --vim.api.nvim_set_hl(0, '@function.builtin', { fg = '#89b4fa' })
 --vim.api.nvim_set_hl(0, '@operator', { fg = '#fab387' })
 --vim.api.nvim_set_hl(0, '@punctuation.delimiter', { fg = '#f9e2af' })
-vim.api.nvim_set_hl(0, '@comment', { fg = '#3b4261' })
+vim.api.nvim_set_hl(0, '@comment', { fg = '#3e4261' }) -- set the color for comments
+vim.api.nvim_set_hl(0, 'Comment', { fg = '#3e4261' }) -- set the color for comments
+vim.api.nvim_set_hl(0, 'NonText', { fg = '#27243b' }) -- set the color for empty lines
+vim.api.nvim_set_hl(0, 'LineNr', { fg = '#27243b' }) -- set the color for line numbers
 
 
 
@@ -233,17 +315,25 @@ require("noice").setup({
 
 -- Line numbers
 vim.wo.number = true
+-- nvim notify options
+require("notify").setup({
+  background_colour = "#26233a",
+})
+
 
 -- Tab and indentation settings
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+-- disable line wrapping
 vim.opt.wrap = false
--- Create an autocommand for VimLeave event
+-- this is for changing the cursor back to the i beam after exiting neovim.
+
+-- Create an augroup named "Shape"
+local shape_group = vim.api.nvim_create_augroup("Shape", { clear = true })
+
+-- Define the autocommand for the VimLeave event
 vim.api.nvim_create_autocmd("VimLeave", {
-    pattern = "*",  -- Trigger for all buffers
-    callback = function()
-        vim.opt.guicursor = ""  -- Equivalent to 'set guicursor='
-        vim.fn.chansend(vim.v.stderr, "\x1b[ q")  -- Correct use of vim.v.stderr
-    end
+  group = shape_group,
+  command = "set guicursor=a:ver90",
 })
